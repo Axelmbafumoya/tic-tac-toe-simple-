e3e8ba1f9dff0ccc1b8e786ba70754403bf7bfc1
MBAFUMOYA <Axelmbafu@email.com>
2026-08-14 01:44:47 +0100

    creat a function who createboard

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameBoard = document.getElementById('game-board');
+const statusMessage = document.getElementById('status-message');
+
+const startCells = ["", "", "", ""]
+
+statusMessage.textContent = "Player X's turn";
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement("div")
+        cellElement.classList.add("square")
+        gameBoard.append(cellElement)
+    })
+}
+
+createBoard()
