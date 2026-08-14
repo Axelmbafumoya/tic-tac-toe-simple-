52e7ad063d92a568b777cd46903b6f786f2d8240
MBAFUMOYA <Axelmbafu@email.com>
2026-08-14 02:37:45 +0100

    style: in style.css, add style allow to create a cross and circle in the site's page

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const gameBoard = document.getElementById('game-board');
 const statusMessage = document.getElementById('status-message');
 
-const startCells = ["", "", "", ""]
+const startCells = ["", "", "", "","","","","",""]
 
 statusMessage.textContent = "Player X's turn";
 
 function createBoard() {
-    startCells.forEach((cell, index) => {
+    startCells.forEach((cell,index) => {
         const cellElement = document.createElement("div")
         cellElement.classList.add("square")
+        const circleElement = document.createElement("div")
+        circleElement.classList.add("cross")
+        cellElement.append(circleElement)
         gameBoard.append(cellElement)
     })
 }
