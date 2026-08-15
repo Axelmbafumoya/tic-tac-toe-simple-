5bb7c41acb7bf233b4594662d471f12a2c2d1cdf
MBAFUMOYA <Axelmbafu@email.com>
2026-08-15 20:39:23 +0100

    feat : creation function addGo and checkSore, addGo function is used to swich bettewen x and o when the use click on box, checkScorre function used to check the winning combination

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 const gameBoard = document.getElementById('game-board');
 const statusMessage = document.getElementById('status-message');
+let go = "circle"
 
-const startCells = ["", "", "", "","","","","",""]
+const startCells = ["", "", "", "", "", "", "", "", ""]
 
 statusMessage.textContent = "Player X's turn";
 
 function createBoard() {
-    startCells.forEach((cell,index) => {
-        const cellElement = document.createElement("div")
-        cellElement.classList.add("square")
-        const circleElement = document.createElement("div")
-        circleElement.classList.add("cross")
-        cellElement.append(circleElement)
-        gameBoard.append(cellElement)
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("square");
+        cellElement.id = index;
+        cellElement.addEventListener("click", addGo)
+        gameBoard.append(cellElement);
     })
 }
 
 createBoard()
+
+function addGo(e) {
+    console.log("click", e.target)
+    const goDisplay = document.createElement("div")
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+    go = go === "circle" ? "cross" : "circle" // if egal the string of "circle" and that is true, it is going to change to be cross , otherwise circle
+    statusMessage.textContent = "it is now " + go + "'s go"
+    e.target.removeEventListener("click", addGo) // when one box get clicked, it remove the addgo function to the clicked box
+    console.log(go)
+    checkScore()
+}
+
+function checkScore() {
+    const allSquares = document.querySelectorAll(".square")
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+
+    winningCombos.forEach(array => {
+        const circleWins = array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains("circle"))
+        if (circleWins) {
+            statusMessage.textContent = "circle wins !"
+        }
+
+    })
+
+    winningCombos.forEach(array => {
+        const crossWins = array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains("cross"))
+        if (crossWins) {
+            statusMessage.textContent = "cross wins !"
+        }
+
+    })
+
+
+
+}
