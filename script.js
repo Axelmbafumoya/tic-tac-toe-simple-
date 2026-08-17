707300c49bd099e7405b0cbab975b18e36aab5c0
MBAFUMOYA <Axelmbafu@email.com>
2026-08-17 21:31:48 +0100

    feat: create button reset and function restGame

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const gameBoard = document.getElementById('game-board');
 const statusMessage = document.getElementById('status-message');
 let go = "circle"
+const resetButton = document.getElementById("reset-button");
 
 const startCells = ["", "", "", "", "", "", "", "", ""]
 
 statusMessage.textContent = "Player X's turn";
 
 function createBoard() {
+
     startCells.forEach((cell, index) => {
+
         const cellElement = document.createElement("div");
         cellElement.classList.add("square");
         cellElement.id = index;
         cellElement.addEventListener("click", addGo)
         gameBoard.append(cellElement);
+
     })
+
 }
 
 createBoard()
 
 function addGo(e) {
+
     console.log("click", e.target)
     const goDisplay = document.createElement("div")
     goDisplay.classList.add(go)
@@ -28,10 +34,13 @@ function addGo(e) {
     e.target.removeEventListener("click", addGo) // when one box get clicked, it remove the addgo function to the clicked box
     console.log(go)
     checkScore()
+
 }
 
 function checkScore() {
+
     const allSquares = document.querySelectorAll(".square")
+
     const winningCombos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
@@ -39,19 +48,25 @@ function checkScore() {
     ]
 
     winningCombos.forEach(array => {
+
         const circleWins = array.every(cell =>
             allSquares[cell].firstChild?.classList.contains("circle"))
+
         if (circleWins) {
             statusMessage.textContent = "circle wins !"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
         }
 
     })
 
     winningCombos.forEach(array => {
+
         const crossWins = array.every(cell =>
             allSquares[cell].firstChild?.classList.contains("cross"))
+            
         if (crossWins) {
             statusMessage.textContent = "cross wins !"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
         }
 
     })
@@ -59,3 +74,13 @@ function checkScore() {
 
 
 }
+
+resetButton.addEventListener("click", resetGame);
+
+function resetGame() {
+    gameBoard.innerHTML = "";
+    go = "circle";
+    statusMessage.textContent = "Circle's turn";
+    createBoard();
+}
+
